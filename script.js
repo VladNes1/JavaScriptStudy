debae1d7c78b492a6a4951dbafebab24b3b3f26d
Vlad Nes <boolsk@gmail.com>
2022-08-25 20:12:25 +0300

    some fix

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log("hello NV");
+
+const clients = [
+
+];
+
+const a = [3,1,1,4,6,8,3,4,2,3,3];
+console.log(a);
+
+// filter всегда возвращает либо тру либо фолс
+let b = a.filter(item => {
+    if (item > 2)
+   return true;
+});
+
+console.log(b);
