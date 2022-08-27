29c08eedfef41205c2f50e2baa0baa7c8f760d04
Vlad Nes <boolsk@gmail.com>
2022-08-27 21:22:00 +0300

    fn post

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-console.log("hello NV");
+const newPost = (post, addDate = Date()) => {
+    return {
+        ...post,
+        addDate
+    }
+}
 
-const clients = [
+const firstPost = {
+    id: 1,
+    name: 'Vlad'
+}
 
-];
+const obj = newPost(firstPost);
 
-const a = [3,1,1,4,6,8,3,4,2,3,3];
-console.log(a);
-
-// filter всегда возвращает либо тру либо фолс
-let b = a.filter(item => {
-    if (item > 2)
-   return true;
-});
-
-console.log(b);
+console.table(obj);
