91cc5236924e8eff0cd29e5adac6f70f7b0c3df7
Vlad Nes <boolsk@gmail.com>
2023-01-15 14:54:16 +0200

    tast 17 done

--- TASKS/17-replace-element-array/finish.js
+++ TASKS/17-replace-element-array/finish.js
@@ -8,3 +8,18 @@
  *
  * 4. Выведите в консоль измененный массив
  */
+
+
+const arrNew = [2, true, "bye"];
+console.log(arrNew);
+
+
+// for (let i = 0; i < arrNew.length; i++) {
+//     console.log(arrNew[i])
+// }
+
+arrNew[1] = false;
+console.log(arrNew);
+// for (let i = 0; i < arrNew.length; i++) {
+//     console.log(arrNew[i])
+// }
