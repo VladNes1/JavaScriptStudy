3c4905da73d13460c5a24b59579f908f89b626a0
Vlad Nes <boolsk@gmail.com>
2023-01-29 19:32:05 +0200

    done 61

--- TASKS/61-sum-numbers/finish.js
+++ TASKS/61-sum-numbers/finish.js
@@ -8,6 +8,16 @@
  * 3. Также верните эту сумму как результат функции
  */
 
+
+function sumNumbers() {
+    let sum = 0;
+    for (let i = 0; i < arguments.length; i++) {
+        sum += arguments[i]
+    }
+    console.log("сума всех аргументов = " + sum)
+    return sum;
+}
+
 sumNumbers(1, 3)
 // 4
 
