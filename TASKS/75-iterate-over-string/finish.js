6480e9221cb54978528a24fd1182e0831a2ee229
Vlad Nes <boolsk@gmail.com>
2023-01-31 20:56:48 +0200

    done 73

--- TASKS/75-iterate-over-string/finish.js
+++ TASKS/75-iterate-over-string/finish.js
@@ -9,6 +9,23 @@ const vowels = ['a', 'e', 'i', 'o', 'u']
 
 const str = 'Today is the best day of my life'
 
+// console.log(str.split(""));
+
+//var1
+// str.split("").forEach((char) => {
+//     if (vowels.includes(char)) {
+//         vowelsCount += 1;
+//     }
+// })
+
+//var 2
+
+for (const char of str) {
+    if (vowels.includes(char)) {
+        vowelsCount += 1;
+    }
+}
+
 // Напишите код здесь
 
 console.log(vowelsCount)
